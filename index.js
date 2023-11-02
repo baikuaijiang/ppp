419a808068fd1fda8690cbbd735ddcc1dd1c5f65
Matthew Dixon <137300280+baikuaijiang@users.noreply.github.com>
2023-11-02 11:21:16 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+}
+#s_top_wrap.s-down{position:fixed;_position:absolute;top:0;left:0;height:70px;z-index:10;width:100%}
+#s_top_wrap .s-center-box{position:relative;z-index:1;width:100%;_width:1000px;height:100%}
+#s_top_wrap.s-down .s-center-box{box-shadow:0 2px 10px 0 rgba(0,0,0,.1);background-color:#fff;border-bottom:1px solid #888\9;_border-bottom:0}
+#s_top_wrap .s-top-nav{position:absolute;top:70px;width:100%;min-width:1250px;_width:1000px;height:40px;overflow:hidden;display:none}
+#s_top_wrap .s-top-nav .s-menus-outer{margin-left:-25px}
+#s_top_wrap.s-down.sam_top_wrap{height:74px}
